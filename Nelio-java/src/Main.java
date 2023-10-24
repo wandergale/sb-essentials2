f66bf56cd9a307bdf131bb835f49467223d4e1a7
Wanderson Ubuntu <wandersongamer2@gmail.com>
2023-10-24 15:42:10 -0300

    logica

--- Nelio-java/src/Main.java
+++ Nelio-java/src/Main.java
@@ -11,6 +11,8 @@ public class Main {
         int idade = sc.nextInt();
 
         System.out.printf("%s tem %d anos de idade%n", nome, idade);
+        System.out.println();
+        System.out.print("Digite o sexo: ");
         char sexo = sc.next().charAt(0);
         System.out.printf("%s tem %d anos de idade e é do sexo %s", nome, idade, sexo);
 
